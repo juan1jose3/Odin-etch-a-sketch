fd7a6217274711f5f5af68c4b462a657b34f308b
juan1jose3 <moralesbenitezjuanjose@gmail.com>
2026-01-13 22:06:21 -0500

    I have to add the display mode

--- js/grids.js
+++ js/grids.js
@@ -30,6 +30,8 @@ function makeGrid(size){
     }
 }
 
+
+
 function getRanNum(){
     return Math.random() * (255 - 120) + 120;
 }
@@ -43,7 +45,13 @@ function getRandomColor(){
 }
 
 function normalMode(toBeMarked){
-    toBeMarked.classList.add("marked");
+    toBeMarked.style.backgroundColor = "white";
+}
+
+function shade(toBeMarked,opacity){
+    toBeMarked.style.backgroundColor = "white";
+    toBeMarked.style.opacity = `${opacity}%`;
+    
 }
 
 function rainBow(toBeMarked){
@@ -53,60 +61,85 @@ function rainBow(toBeMarked){
 
 
 function sketch(){
-    makeGrid(16);
+    let gridSize = 16;
+    makeGrid(gridSize);
 
     let mode = "regular";
+    let shadeOpacity = 0;
+
     let erase = document.querySelector(".erase");
     let resizeGrid = document.querySelector(".resizeGrid");
     let regularMode = document.querySelector(".regularMode");
     let rainbowMode = document.querySelector(".rainbowMode"); 
+    let shadeMode = document.querySelector(".shadeMode");
+
+    let displaySize = document.querySelector(".displaySize");
+
+    let displayMode = document.querySelector(".displayMode");
+
     
+    
+    displayMode.textContent = mode;
+    displaySize.textContent = `Size: ${gridSize} X ${gridSize}`;
     innerGrid.addEventListener("mouseover",(event) =>{
         let toBeMarked = event.target;
         //console.log(toBeMarked);
-
+        
         if(mode == "regular"){
             normalMode(toBeMarked);
         }else if(mode == "rainBow"){
             rainBow(toBeMarked);        
+        }else if(mode == "shade"){
+            if(shadeOpacity == 100){
+                shadeOpacity = 0;
+            }
+            shade(toBeMarked,shadeOpacity);
+            shadeOpacity += 10;
+            
         }
+
+        erase.addEventListener("click",()=>{
+            toBeMarked.style.backgroundColor = "";
+            toBeMarked.style.opacity = "";
+            
+        });
         
-    
+        
     });
-
-
+    
+   
+    
     regularMode.addEventListener("click",()=>{
         mode = "regular";
+        displayMode.textContent = mode;
     });
+
+    shadeMode.addEventListener("click",()=>{
+        mode = "shade";
+        displayMode.textContent = mode;
+    });
+
     rainbowMode.addEventListener("click",() =>{
         mode = "rainBow";
+        displayMode.textContent = mode;
+        
     });
+
     
     
+
     resizeGrid.addEventListener("click",() =>{
-        let gridSize = parseInt(prompt("type the size you want :), (min 2,max 100) "));
+        gridSize = parseInt(prompt("type the size you want :), (min 2,max 100) "));
        
         if(gridSize >= 2 && gridSize <= 100 ){
             squaresContainer.remove();
             makeGrid(gridSize);
+            displaySize.textContent = `Size: ${gridSize} X ${gridSize}`;
         }
     });
 
-    // I must fix this next
-    /*
-    erase.addEventListener("click",() =>{
-        toBeMarked.classList.remove("marked");
-    });
-
-    */
-    
-    
     
 
-    
-
-    
-    
 
 }
 
